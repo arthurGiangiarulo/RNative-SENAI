acd6ec1c7fe695441c340426933e97285d1e87e0
Arthur Giangiarulo <111184548+arthurGiangiarulo@users.noreply.github.com>
2024-10-11 21:01:29 -0300

    useRoute

--- src/screens/AlunoDetalhes.js
+++ src/screens/AlunoDetalhes.js
@@ -1,15 +1,19 @@
 import { Text, View, StyleSheet } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
-export default function AlunoDetalhes( { aluno }) {
+export default function AlunoDetalhes() {
+    const route = useRoute();
+    const { aluno } = route.params;
+
     return (
         <View style={styles.caixa} >
             <Text style={styles.titulo}>Detalhes do aluno</Text>
             <Text style={styles.emoji}>'😎'</Text>
-            <Text>Nome: Yuri</Text>
-            <Text>Idade: 20</Text>
-            <Text>Curso: Sistemas de Informação</Text>
-            <Text>Endereço: Rua dos Yuri, nº 0</Text>
-            <Text>Telefone: (99) 99999-9999</Text>
+            <Text>Nome: {aluno.nome}</Text>
+            <Text>Idade: {aluno.idade}</Text>
+            <Text>Curso: {aluno.curso}</Text>
+            <Text>Endereço: {aluno.endereco}</Text>
+            <Text>Telefone: {aluno.telefone}</Text>
         </View>
     );
 }
